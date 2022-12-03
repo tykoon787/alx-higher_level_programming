2d394ea01b20919b7720f96c9371f16c385b1e82
tykoon787 <tykoon787@protonmail.com>
2022-12-03 17:17:20 +0300

    Updated code

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     for letters in range(ord('a'), ord('z') + 1):
-        print("{:c}".format(letters, end=" "))
         if letters == ord(c):
             return True
         else:
