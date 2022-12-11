8c835e145ec5c4c78e664ff27da9521082e0bedf
tykoon787 <tykoon787@protonmail.com>
2022-12-11 08:23:32 +0300

    Updated code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if "__name__" == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    a = 1
+    b = 2
 
-print(f'{a} + {b} = {add(a,b)}')
+    print(f'{a} + {b} = {add(a,b)}')
