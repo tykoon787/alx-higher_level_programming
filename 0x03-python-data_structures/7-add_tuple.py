3e3bb68bed6ab6918b7bdfdd20b83c3b29ad5257
tykoon787 <tykoon787@protonmail.com>
2022-12-14 17:27:39 +0300

    Updated code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,4 +15,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_b) > 2:
         tuple_b = tuple_b[:2]
     res = tuple(sum(item) for item in zip(tuple_a, tuple_b))
-    return
+    return (res)
