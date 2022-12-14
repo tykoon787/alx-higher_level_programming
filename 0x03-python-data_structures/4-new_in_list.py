501b333071b983281c1dff76a5df3cdc41d80b51
tykoon787 <tykoon787@protonmail.com>
2022-12-14 15:41:25 +0300

    coded

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list
+    if idx < 0:
+        return (my_list)
+    elif (idx >= len(my_list)):
+        return (list_cpy)
+    else:
+        list_cpy[idx] = element
+        return (list_cpy)
