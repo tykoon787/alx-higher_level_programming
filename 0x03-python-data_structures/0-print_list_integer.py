5fe90206c8fdeabaad78e3f235af653c3d36086b
tykoon787 <tykoon787@protonmail.com>
2022-12-14 14:13:39 +0300

    Updated code

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    def print_list_integrity(my_list=[]):
+    def print_list_integer(my_list=[]):
         for x in my_list:
             print("{:d}".format(x))
