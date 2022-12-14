49ade7fecc8481e642bf766622bf4ff32da5db6a
tykoon787 <tykoon787@protonmail.com>
2022-12-14 15:43:07 +0300

    Updated code

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     list_cpy = my_list
     if idx < 0:
-        return (my_list)
+        return (list_cpy)
     elif (idx >= len(my_list)):
         return (list_cpy)
     else:
