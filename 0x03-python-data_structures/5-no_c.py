ad792a988eeb95fbee6fedd1b6f54398119d74b9
tykoon787 <tykoon787@protonmail.com>
2022-12-14 16:09:00 +0300

    Coded

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    str_list = list(my_string)
+    index = 0 
+    for x in str_list:
+        if x == 'c' or x == 'C':
+            str_list[index] = ""
+        index += 1
+    return "".join(str_list)
