937403bbcc72134902917c7ef463bbb377c44d00
tykoon787 <tykoon787@protonmail.com>
2022-12-14 14:45:23 +0300

    Updated code

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return None
+        return
     elif (idx >= len(my_list)):
-        return None
+        return
     else:
-        print("Element at index {:d} is {:d}".format(idx, element_at(my_list, idx)))
+        return (my_list[idx])
