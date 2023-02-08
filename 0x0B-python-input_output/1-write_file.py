6e25c831dc25713f866c60d0369ed74d3745879f
tykoon787 <tykoon787@protonmail.com>
2023-02-08 14:14:59 +0300

    Updated py code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,8 @@ Function to write a string to a  file in UTF8-Encoding
 
 
 def write_file(filename="", text=""):
+    """
+    Write `text` to `filename`, crete new file if `filename` doesn't exist
+    """
     with open(filename, mode="w", encoding="utf-8") as fileName:
         fileName.write(text)
