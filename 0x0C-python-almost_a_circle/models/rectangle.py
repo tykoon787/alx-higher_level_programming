86ee1e9930ab99d34b22162dd693a305f26ca958
tykoon787 <tykoon787@protonmail.com>
2023-02-08 18:58:56 +0300

    Updated code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -43,7 +43,7 @@ class Rectangle(Base):
             raise TypeError("{} must be an integer".format(value))
         elif(value < 0):
             raise ValueError("{} must be > 0".format(value))
-        self.__width = value 
+        self.__width = value
 
     @property
     def height(self):
