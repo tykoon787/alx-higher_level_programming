e37ceb6b141e5a13c26bc5599df2d0a62117ff1c
tykoon787 <tykoon787@protonmail.com>
2023-02-08 16:31:03 +0300

    updated py code

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,6 +11,9 @@ import json
 
 
 def load_from_json_file(filename):
+    """
+    Function that returns an object from a file
+    """
     with open(filename, mode="r", encoding="utf-8") as f:
         data_read = f.read()
         json.loads(data_read)
