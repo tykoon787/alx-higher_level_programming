a64febc257b37d8cb4427dc8f2ef1749399ad2e5
tykoon787 <tykoon787@protonmail.com>
2023-02-08 15:23:11 +0300

    Updated py code

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -6,4 +6,7 @@ A function that returs the JSON Rep of an object(string)
 
 
 def to_json_string(my_obj):
+    """
+    Function that returns the json representation of an object
+    """
     return json.dumps(my_obj)
