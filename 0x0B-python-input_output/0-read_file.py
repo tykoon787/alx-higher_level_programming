66064ad32176dcb7dbe6aa31221f6ba0c8c8dfcd
tykoon787 <tykoon787@protonmail.com>
2023-02-08 14:10:43 +0300

    Updated py code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -11,11 +11,15 @@ def read_file(filename=""):
     Reads a file, filename
     """
     with open(filename, mode="r", encoding="utf-8") as fileName:
+        # Run until you find a break statement
         while True:
+            # split the lint into a list
             line = fileName.read().splitlines()
+            # Check for an empty line
             if not line:
                 break
             else:
+                # Loop throught the list containing the string while printinh
                 for i in line:
                     if i != "":
                         print(i)
