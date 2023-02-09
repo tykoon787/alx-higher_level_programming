5daf8184a6db5b9be8116241e2bbc94218cdbdaa
tykoon787 <tykoon787@protonmail.com>
2023-02-09 14:25:48 +0300

    updated py code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,50 +32,32 @@ class Square(Rectangle):
     """
 
     def __init__(self, size, x=0, y=0, id=None):
-        self.__size = size
-        self.__x = x
-        self.__y = y
-        self.id = id
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
-        print("Value Received: {}".format(value))
-        if type(value) != int:
-            raise TypeError("Size must be an integer")
-        if value <= 0:
-            raise ValueError("Size must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     @property
     def x(self):
-        return self.__x
+        return self.x
 
     @x.setter
     def x(self, value):
-        print("Value Received: {}".format(value))
-        if type(value) != int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
+        self.x = value
 
     @property
     def y(self):
-        return self.__y
+        return self.y
 
     @y.setter
     def y(self, value):
-        print("Value Received: {}".format(value))
-        if type(value) != int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
+        self.y = value
 
     def __str__(self):
         """
@@ -84,6 +66,6 @@ class Square(Rectangle):
         return ("[{}] ({}) {}/{} - {}".format(
             __class__.__name__,
             self.id,
-            self.__x,
-            self.__y,
-            self.__size))
+            self.x,
+            self.y,
+            self.size))
