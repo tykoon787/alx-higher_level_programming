e1c1d351a14ba4a3860cf8c7ab8a0c71affa9664
tykoon787 <tykoon787@protonmail.com>
2023-02-09 12:02:46 +0300

    updated py code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -106,9 +106,13 @@ class Rectangle(Base):
         """
         Returns formatted information to display
         """
-        return ("[{}] ({}) <{}>/<{}> - <{}>/<{}>".format(__class__.__name__, self.id,
-                                                         self.__x, self.__y, 
-                                                         self.__width, self.__height))
+        return ("[{}] ({}) <{}>/<{}> - <{}>/<{}>".format(
+            __class__.__name__,
+            self.id,
+            self.__x,
+            self.__y,
+            self.__width,
+            self.__height))
 
     def update(self, *args):
         """
