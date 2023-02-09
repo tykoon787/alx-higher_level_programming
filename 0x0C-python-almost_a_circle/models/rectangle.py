6a966c5f35352c1f454f0755002c4a324a6ab13f
tykoon787 <tykoon787@protonmail.com>
2023-02-09 14:54:04 +0300

    updated py code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -136,3 +136,30 @@ class Rectangle(Base):
                 pass
         else:
             print()
+
+    def to_dictionary(self):
+        """
+        Function that returs the dictionary representation of an object
+
+            Parameters:
+                None
+
+            Return:
+                Dictionary representation of a rectangle
+        """
+        return (self.__dict__)
+
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(10, 2, 1, 9)
+    print(r1)
+    r1_dictionary = r1.to_dictionary()
+    print(r1_dictionary)
+    print(type(r1_dictionary))
+
+    r2 = Rectangle(1, 1)
+    print(r2)
+    r2.update(**r1_dictionary)
+    print(r2)
+    print(r1 == r2)
