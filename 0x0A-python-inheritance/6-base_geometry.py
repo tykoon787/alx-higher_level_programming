2de055b588f1189a4ae39770f0e76e0fa43a3775
tykoon787 <tykoon787@protonmail.com>
2023-02-12 10:20:57 +0300

    updated py code

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Base Class
+
+    Methods:
+        area(self): Calculates Area
+"""
+
+
+class BaseGeometry():
+    """
+    Class Base Geometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
