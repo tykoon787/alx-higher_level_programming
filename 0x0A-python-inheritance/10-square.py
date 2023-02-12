8c0bb7875b2f69f7a0a540da6f891d654c4bd129
tykoon787 <tykoon787@protonmail.com>
2023-02-12 11:34:46 +0300

    updated py code

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A square inheriting from the rectangle class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        super().integer_validator(size)
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the area of a square
+        """
+        return (self.__size * self.__size)
