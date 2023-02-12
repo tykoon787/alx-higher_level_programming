0397b2093fef3872a1075dd558f9d8a04bf0c99d
tykoon787 <tykoon787@protonmail.com>
2023-02-12 10:01:41 +0300

    updated py code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,7 +12,7 @@ def inherits_from(obj, a_class):
     class that inherited (directly or indirectly) from the specified
     class ; otherwise False
     """
-    if isinstance(obj, a_class) or type(obj) is a_class:
-        return True
-    else:
+    if type(obj) is a_class or not isinstance(obj, a_class):
         return False
+    else:
+        return True
