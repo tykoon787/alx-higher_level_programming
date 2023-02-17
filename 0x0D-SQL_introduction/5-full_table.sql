cce1923261042a15c003e7062c76fc2ff6068133
tykoon787 <tykoon787@protonmail.com>
2023-02-17 16:26:55 +0300

    SQL Script

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Script that prints the ful description of the table
-SELECT column_name, data_type, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_name = 'first_table';
+SHOW CREATE TABLE first_table;
