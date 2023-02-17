1e89650acd11155fb6d079fe8e64a46934ed665e
tykoon787 <tykoon787@protonmail.com>
2023-02-17 18:16:14 +0300

    SQL Script

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Script that lists all records of the second_table
-SELECT score, name COUNT(name) GROUP BY score ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
