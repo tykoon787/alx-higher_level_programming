2c4617bb85f34808f988294fbf96bb04ad15a4bc
tykoon787 <tykoon787@protonmail.com>
2023-02-22 07:58:47 +0300

    SQL Script

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Script that creates a user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
+CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL
 ON *.*
 TO 'user_0d_1'@'localhost';
