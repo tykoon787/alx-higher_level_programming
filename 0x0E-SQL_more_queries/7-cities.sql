7c18cc9c2719d1c80e39318e704ee0e7a2ddf753
tykoon787 <tykoon787@protonmail.com>
2023-02-22 09:05:16 +0300

    SQL Script

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
     id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.state(state_id))
-    type=InnoDB;
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(state_id)
+);
