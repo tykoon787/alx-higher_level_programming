0e07f255e61fd692a944b470267e725c27ef0aac
tykoon787 <tykoon787@protonmail.com>
2023-02-22 08:49:52 +0300

    SQL Script

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Script that creates a database usa and a table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS htbn_0d_usa.states(
-    id INT PRIMARY KEY NOT NULL AUTO_INCRIMENT,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL
 );
