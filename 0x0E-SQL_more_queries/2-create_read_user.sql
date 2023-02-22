a91143b7eb5a85a7ac0e74bc0758561c06651248
tykoon787 <tykoon787@protonmail.com>
2023-02-22 08:26:30 +0300

    SQL Script

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Script that creates a database and grants select privilege to the user
-CREATE DATABASE IF NOT EXISTS hbtn_02;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-ON hbtn_02.*
+ON hbtn_0d_2.*
 TO 'user_0d_2'@'localhost';
