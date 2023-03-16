2b8b1d0ea4538538075eba809671a6cf8819ce79
tykoon787 <tykoon787@protonmail.com>
2023-03-16 10:27:44 +0300

    Updated code

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,14 @@
 const [firstArg] = process.argv.slice(2);
 let num = parseInt(firstArg);
 
-if (parseInt(firstArg)){
-    for (let width = num; width > 0; width--){
-        let row = '';
-        for (let height = num; height > 0; height--){
-            row += 'x';
-        }
-        console.log(row);
+if (parseInt(firstArg)) {
+  for (let width = num; width > 0; width--) {
+    let row = '';
+    for (let height = num; height > 0; height--) {
+      row += 'x';
     }
+    console.log(row);
+  }
 } else {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 }
