614a366ddf19f6ed6118eaab8936fb1790ccbb5e
tykoon787 <tykoon787@protonmail.com>
2023-03-16 11:03:17 +0300

    Updated code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 const [firstArg] = process.argv.slice(2);
 
-function factorial (n) {
+function factorial(n) {
   if (isNaN(n)) {
     return 1;
   } else if (n < 0) {
     return NaN;
+  } else if (n === 0) {
+    return 1;
   } else {
-    let result = 1;
-    for (let i = n; i > 0; i--) {
-      result *= i;
-    }
-    return result;
+    return n * factorial(n - 1);
   }
 }
 console.log(factorial(firstArg));
