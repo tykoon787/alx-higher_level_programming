0fb004f7ee40323a2d0c7b8efbd715db46d1da0d
tykoon787 <tykoon787@protonmail.com>
2023-03-16 20:00:16 +0300

    Created Exectutable Script '6-square.js'

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const SquareClass = require('./5-square');
+
+class Square extends SquareClass {
+  charPrint(c) {
+    if (!c) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.export = Square;
