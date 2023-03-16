9eb73236f2244f6d555cef41c50e03bb8dc696d3
tykoon787 <tykoon787@protonmail.com>
2023-03-16 09:59:21 +0300

    Updated code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const [firstArg] = process.argv.slice(2);
 
 if (parseInt(firstArg)) {
-    console.log(`My Number: ${parseInt(firstArg)}`);
+    console.log(`My number: ${parseInt(firstArg)}`);
 } else {
     console.log('Not a number');
 }
