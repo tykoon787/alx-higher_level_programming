c917fea6a89630ea23547aa182ca4fc4e178a7c1
tykoon787 <tykoon787@protonmail.com>
2023-03-16 20:13:39 +0300

    Created Exectutable Script '7-occurrences.js'

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function nbOccurences (list, searchElement) {
+  const len = list.length;
+  let count = 0;
+  for (let i = 0; i < len; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+module.exports = nbOccurences;
