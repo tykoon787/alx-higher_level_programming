703646cd0304ea2dbb6f2843cc009ce8bd2bfab7
tykoon787 <tykoon787@protonmail.com>
2023-03-16 09:20:32 +0300

    Updated code

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,5 +4,5 @@ const [firstArg, ...restArgs] = process.argv.slice(2);
 if (firstArg !== undefined) {
     console.log(firstArg);
 } else {
-    console.log('No argument')
+    console.log('No argument');
 }
