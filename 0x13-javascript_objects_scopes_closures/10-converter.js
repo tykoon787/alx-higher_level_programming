dc67b03283e7ef0acc592cee19c288913b1913d0
tykoon787 <tykoon787@protonmail.com>
2023-03-16 20:42:50 +0300

    Updated code

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
-exports.converter = function (base) {
+function converter (base) {
   return function (num) { return num.toString(base); };
-};
+}
+
+exports.converter = converter;
