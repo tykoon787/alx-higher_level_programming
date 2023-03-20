a781ef2725e362a7a03af09ec831de7970a15faf
tykoon787 <tykoon787@protonmail.com>
2023-03-20 18:08:59 +0300

    updated py code

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A script that selects all states from the states
+Table that start with N
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    user_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=user_name,
+                         password=user_pass,
+                         db=db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states where name LIKE 'N%' ORDER BY states.id ASC")
+    results = cursor.fetchall()
+    for row in results:
+        print("%s" % (row,))
+
+    cursor.close()
+    db.close()
