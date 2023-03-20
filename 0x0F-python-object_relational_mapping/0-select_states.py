2e175d4810effa6ddd0610312379a9cb62e0ef1f
tykoon787 <tykoon787@protonmail.com>
2023-03-20 18:03:07 +0300

    updated py code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     user_name = sys.argv[1]
     user_pass = sys.argv[2]
     db_name = sys.argv[3]
-    db = MySQLdb.connect(host="localhost:3306", user=user_name,
+    db = MySQLdb.connect(host="localhost", port=3306, user=user_name,
                          password=user_pass, db=db_name)
 
     cursor = db.cursor()
@@ -20,4 +20,6 @@ if __name__ == "__main__":
     results = cursor.fetchall()
     for row in results:
         print("%s" % (row,))
+
+    cursor.close()
     db.close()
