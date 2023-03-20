33b6ae20ccb854b055c5a418ee24b7a63748eac6
tykoon787 <tykoon787@protonmail.com>
2023-03-20 17:44:38 +0300

    updated py code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,17 @@ import MySQLdb
 A script that selects all states from the states
 Table
 """
-user_name = sys.argv[1]
-user_pass = sys.argv[2]
-db_name = sys.argv[3]
-db = MySQLdb.connect(host="localhost:3306", user=user_name,
-                     password=user_pass, db=db_name)
+if __name__ == "__main__":
+    user_name = sys.argv[1]
+    user_pass = sys.argv[2]
+    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost:3306", user=user_name,
+                         password=user_pass, db=db_name)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-results = cursor.fetchall()
-for id, name in results:
-    print("(%s, '%s')" % (id, name))
-db.close()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    results = cursor.fetchall()
+    for id, name in results:
+        print("(%s, '%s')" % (id, name))
+    db.close()
