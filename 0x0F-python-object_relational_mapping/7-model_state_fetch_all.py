f6e406a406aee28dffae2cedfe109a3f575ee030
tykoon787 <tykoon787@protonmail.com>
2023-03-20 21:14:24 +0300

    updated py code

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,27 +8,29 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
-# Assign arguments
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-mysql_db = sys.argv[3]
+if __name__ == "__main__":
 
-# Create the engine
-engine = create_engine('mysql+mysqldb://%s:%s@localhost/%s' %
-                       (mysql_username, mysql_password, mysql_db),
-                       pool_pre_ping=True)
-Base.metadata.create_all(engine)
+    # Assign arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
 
-# Create a session to query the db
-Session = sessionmaker(bind=engine)
+    # Create the engine
+    engine = create_engine('mysql+mysqldb://%s:%s@localhost/%s' %
+                           (mysql_username, mysql_password, mysql_db),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-# Instantiate the session
-session = Session()
+    # Create a session to query the db
+    Session = sessionmaker(bind=engine)
 
-# Query to get all results
-results = session.query(State).all()
-for result in results:
-    print("{}: {}".format(result.id, result.name))
+    # Instantiate the session
+    session = Session()
 
-# Close Session
-session.close()
+    # Query to get all results
+    results = session.query(State).all()
+    for result in results:
+        print("{}: {}".format(result.id, result.name))
+
+    # Close Session
+    session.close()
