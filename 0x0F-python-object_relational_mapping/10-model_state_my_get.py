b2006a2dbc4071a620591c0e00713531e6d6912c
tykoon787 <tykoon787@protonmail.com>
2023-03-21 12:04:01 +0300

    updated py code

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that lists the state objects with name
+passed as an argument
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    # Assign arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+    state_search = sys.argv[4]
+
+    # Create the engine
+    engine = create_engine('mysql+mysqldb://%s:%s@localhost/%s' %
+                           (mysql_username, mysql_password, mysql_db),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session to query the db
+    Session = sessionmaker(bind=engine)
+
+    # Instantiate the session
+    session = Session()
+
+    # Query to get all results
+    results = session.query(State).filter_by(name='%s' % (state_search,))
+    for result in results:
+        print(result.id)
+
+    # Close Session
+    session.close()
