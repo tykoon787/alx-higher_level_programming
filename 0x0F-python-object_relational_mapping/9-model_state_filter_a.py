f655c77765f5455d8bc31c5f51020fd14ac54dc4
tykoon787 <tykoon787@protonmail.com>
2023-03-21 11:51:37 +0300

    updated py code

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that lists the state objects starting with 'a'
+the database
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    # Assign arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    # Create the engine
+    engine = create_engine('mysql+mysqldb://%s:%s@localhost/%s' %
+                           (mysql_username, mysql_password, mysql_db),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # Create a session to query the db
+    Session = sessionmaker(bind=engine)
+
+    # Instantiate the session
+    session = Session()
+
+    # Query to get all results
+    results = session.query(State).filter(
+        State.name.ilike("%a%")).order_by(State.id).all()
+    for result in results:
+        print("{}: {}".format(result.id, result.name))
+
+    # Close Session
+    session.close()
