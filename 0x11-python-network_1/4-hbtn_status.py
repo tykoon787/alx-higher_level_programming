340c44d542929dcaae9f116d4133c8245bcceebb
tykoon787 <tykoon787@protonmail.com>
2023-04-01 02:56:03 +0300

    Coded 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alx-intranet.hbtn.io/status.
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
