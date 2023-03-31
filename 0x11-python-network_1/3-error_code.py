6bbcd130a5c0969fc4167dff33dc35fe4e979878
tykoon787 <tykoon787@protonmail.com>
2023-04-01 02:56:01 +0300

    Coded 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """
-script that takes in a URL, sends a request to the URL and displays the
+Script that takes in a URL, sends a request to the URL and displays the
 body of the response (decoded in utf-8).
-You have to manage urllib.error.HTTPError exceptions and
-print: Error code: followed by the HTTP status code.
+
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
 """
-import urllib.request
-import urllib.error
-import sys
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
+    url = argv[1]
+    req = Request(url)
+
     try:
-        with urllib.request.urlopen(url) as response:
-            response_text = response.read().decode("utf-8")
-            print(response_text)
-    except urllib.error.HTTPError as e:
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except HTTPError as e:
         print("Error code: {}".format(e.code))
