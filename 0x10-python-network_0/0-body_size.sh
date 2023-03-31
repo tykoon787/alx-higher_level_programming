667e065c5af2dec677bdb800e88461a34d85f24b
tykoon787 <tykoon787@protonmail.com>
2023-03-31 22:47:28 +0300

    Updated code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that displays the size of the body of the response
-URL=$1
-curl -sI "$URL" | grep Content-Length | grep -oE '[0-9]+' | awk '{print $1}'
+curl -sI "$1" | grep Content-Length | grep -oE '[0-9]+' | awk '{print $1}'
