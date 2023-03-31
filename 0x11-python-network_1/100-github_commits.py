bf5169425e1ccedc8a5bd4e4a200c600adc04aa4
tykoon787 <tykoon787@protonmail.com>
2023-04-01 02:55:56 +0300

    Coded 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """
-script that takes 2 arguments in order to list 10 commits (from the most
-recent to oldest) of the repository "rails" by the user "rails".
-Print all commits by: `<sha>: <author name>` (one by line)
-The first argument will be the repository name
-The second argument will be the owner name
+Python script that takes 2 arguments in order to solve this challenge.
+
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-import sys
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    req = requests.get(url)
+    commits = req.json()
+
     try:
-        repo_name = sys.argv[1]
-        username = sys.argv[2]
-        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
-            .format(username, repo_name)
-        response = requests.get(commmits_url)
-        json_obj = response.json()
-        for i, obj in enumerate(json_obj):
-            if i == 10:
-                break
-            if type(obj) is dict:
-                name = obj.get('commit').get('author').get('name')
-                print("{}: {}".format(obj.get('sha'), name))
-    except ValueError as invalid_json:
+        for index in range(10):
+            print("{}: {}".format(
+                commits[index].get("sha"),
+                commits[index].get("commit").get("author").get("name")))
+    except IndexError:
         pass
