e4b42f7b92c6ad0998565a220562fbe469d6edfc
tykoon787 <tykoon787@protonmail.com>
2023-03-31 23:43:46 +0300

    Bash Script

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get request to the URL and display the body of the response
-curl -sLX PUT -d "user_id=98" -H "Origin: ALXSchool" 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -d "user_id=98" -H "Origin:School"
