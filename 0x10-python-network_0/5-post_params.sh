85c44e9a0d417f28dc1fe22dfacf60aec16823bb
tykoon787 <tykoon787@protonmail.com>
2023-03-31 23:26:54 +0300

    Updated code

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST Request
-curl -sX -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
