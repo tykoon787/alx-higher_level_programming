1445867fe545296ddef57235712ac83c97f6c1fb
tykoon787 <tykoon787@protonmail.com>
2023-05-02 19:49:52 +0300

    Coded JS 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,8 +8,8 @@ if (len <= 2) {
   console.log(`Usage: ${path.basename(argv[1])} <file path>`);
 } else {
   const file = path.resolve(argv[2]);
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) throw (err);
+  fs.readFile(path.basename(file), 'utf8', (err, data) => {
+    if (err) console.log(err);
     else console.log(data);
   });
 }
